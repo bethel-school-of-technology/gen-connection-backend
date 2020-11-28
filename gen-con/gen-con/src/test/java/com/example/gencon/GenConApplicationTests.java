d2ef382936eec0da2d1b3bb363b4daf37875d7d8
husspnw <73805535+husspnw@users.noreply.github.com>
2020-11-28 15:38:04 -0800

    Add files via upload

---
+++ gen-con/gen-con/src/test/java/com/example/gencon/GenConApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gencon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenConApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
