b695b2c5935d47b8fd91a09e11dab7665d8b3002
HUSS <husspnw@gmail.com>
2020-12-08 21:33:03 -0800

    Author, Authen, Security, MySql

---
+++ src/main/java/com/gencon/auth/genconApplication.java
@@ -0,0 +1,13 @@
+package com.gencon.auth;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class genconApplication {
+    public static void main (String[] args) {
+        SpringApplication.run(genconApplication.class, args);
+    }
+}
